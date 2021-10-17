6a4b06595758be166f2b46a2ce2121069f54bf9c
belbachir <belbachirabderrahman@gmail.com>
2021-10-17 07:02:53 +0100

    refactoring

---
+++ init_project.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+printf "Cloning laradock ...\n"
+git clone https://github.com/Laradock/laradock.git
+
+printf "go to laradock ...\n"
+cd laradock
+
+printf "Rename .env.example to .env ...\n"
+cp .env.example .env
+
+printf "launch docker-composer build ... \n" ;
+docker-compose build nginx mysql phpmyadmin redis workspace
+
+printf "launch docker-composer up -d ... \n" ;
+docker-compose up -d nginx mysql phpmyadmin redis workspace
+
+printf "Generating project key ...\n" ;
+php artisan key:generate
+
+printf "Executing migration ...\n" ;
+php artisan migrate
