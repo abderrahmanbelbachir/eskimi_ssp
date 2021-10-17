6cfec2c25e04f474fab7754199bc7e0ae128e61d
belbachir <belbachirabderrahman@gmail.com>
2021-10-17 07:59:28 +0100

    update init_project bash file

--- init_project.sh
+++ init_project.sh
@@ -3,13 +3,17 @@
 printf "composer install ... \n" ;
 composer install
 
+printf "Rename .env.example to .env for eskimi project ...\n"
+cp .env.example .env
+
+
 printf "Cloning laradock ...\n"
 git clone https://github.com/Laradock/laradock.git
 
 printf "go to laradock ...\n"
 cd laradock
 
-printf "Rename .env.example to .env ...\n"
+printf "Rename .env.example to .env for laradock ...\n"
 cp .env.example .env
 
 printf "launch docker-composer build ... \n" ;
@@ -18,11 +22,6 @@ docker-compose build nginx mysql phpmyadmin redis workspace
 printf "launch docker-composer up -d ... \n" ;
 docker-compose up -d nginx mysql phpmyadmin redis workspace
 
-printf "docker-compose exec workspace bash ... \n" ;
-docker-compose exec workspace bash
+printf "docker-compose exec workspace and run artisan migrate ... \n" ;
+docker-compose exec workspace php artisan migrate
 
-printf "Generating project key ...\n" ;
-php artisan key:generate
-
-printf "Executing migration ...\n" ;
-php artisan migrate
